c700ee38f2f96d5d81a535a21a06afa9ae892ebf
festinagashi <festina.gashi6@student.uni-pr.edu>
2022-09-17 13:44:55 -0700

    getting data from API

---
+++ IosApp/WebService/ApiCall.swift
@@ -0,0 +1,31 @@
+//
+//  ApiCall.swift
+//  IosApp
+//
+//  Created by TestPc on 17.9.22.
+//
+
+import Foundation
+
+enum NetworkError:Error{
+    case badUrl
+    case invalidRequest
+}
+
+
+class ApiCall{
+    
+    func getAllVegetables(url: URL?) async throws -> [Vegetable]{
+        guard let url = url else{
+            throw NetworkError.badUrl
+        }
+       let(data,response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else{
+            throw NetworkError.invalidRequest
+        }
+        let vegetables = try? JSONDecoder().decode([Vegetable].self, from: data)
+        return vegetables ?? []
+    }
+    
+}
